094c8dafca966ddd25e75962c514cf480c9cd7a7
David <delutilo.dev@gmail.com>
2024-05-30 14:35:44 -0500

    create helper function for setting active tab

--- public/helpers/animation.js
+++ public/helpers/animation.js
@@ -1,11 +1,35 @@
 function highlightMenuItem(slider, item) {
-    let x = item.offsetLeft;
-    let y = item.offsetTop;
-    let w = item.clientWidth;
-    let h = item.offsetHeight;
+  let x = item.offsetLeft;
+  let y = item.offsetTop;
+  let w = item.clientWidth;
+  let h = item.offsetHeight;
 
-    slider.style.left = `${x}px`;
-    slider.style.top = `${y}px`;
-    slider.style.width = `${w}px`;
-    slider.style.height = `${h}px`;
+  slider.style.left = `${x}px`;
+  slider.style.top = `${y}px`;
+  slider.style.width = `${w}px`;
+  slider.style.height = `${h}px`;
+}
+
+function animateActiveTab(nav, navItem, scrollDistance) {
+  let activeItem = nav.querySelector('.active');
+
+  if (activeItem === navItem) {
+    return activeItem;
   }
+
+  activeItem.classList.remove('active');
+  navItem.classList.add('active');
+
+  let distanceFromTitle = Math.abs(navItem.offsetTop - scrollDistance);
+
+  let itemOrder = parseInt(navItem.dataset.order);
+  let activeItemOrder = parseInt(activeItem.dataset.order);
+
+  if (itemOrder > activeItemOrder) {
+    distanceFromTitle = -distanceFromTitle;
+  }
+
+  nav.style.transform = `translateY(${distanceFromTitle}px)`;
+
+  return navItem
+}
