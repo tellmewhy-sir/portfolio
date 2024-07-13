2abfc0b0e0c4b5d54d51b2cb5349128cd732d448
David <delutilo.dev@gmail.com>
2024-07-12 21:11:03 -0500

    replace highlight element with clip path
    referencing method showcased at https://emilkowal.ski/ui/the-magic-of-clip-path

--- src/components/react/Navigation/index.jsx
+++ src/components/react/Navigation/index.jsx
@@ -10,6 +10,7 @@ export default function Navigation({ links }) {
   const nameRef = useRef(null);
   const navRef = useRef(null);
   const menuRef = useRef(null);
+  const menuActiveRef = useRef(null);
   const highlightRef = useRef(null);
 
   useEffect(() => {
@@ -18,24 +19,29 @@ export default function Navigation({ links }) {
     let activeItem = navRef.current.querySelector('.active');
 
     if (window.screen.width > 768) {
-      highlightMenuItem(activeItem);
+      // highlightMenuItem(activeItem);
     } else {
-      highlightMenuItem(nameRef.current);
+      // highlightMenuItem(nameRef.current);
     }
     // let menuCenter = navRef.current.offsetTop;
     // setMenuCenter(menuCenter);
   }, [currActive, setCurrActive]);
 
   const highlightMenuItem = (item) => {
-    let x = item.offsetLeft;
-    let y = item.offsetTop;
-    let w = item.clientWidth;
-    let h = item.offsetHeight;
+    const menu = menuActiveRef.current;
+    
+    const {offsetTop, offsetHeight, offsetWidth, offsetLeft} = item;
+    const clipTop = offsetTop
+    const clipBottom = offsetTop + offsetHeight;
+    const clipLeft = offsetLeft;
+    const clipRight = offsetLeft + offsetWidth;
+  
+    const insetTop = Number((clipTop / menu.offsetHeight) * 100).toFixed();
+    const insetRight = Number(100 - ((clipRight+16) / menu.offsetWidth) * 100).toFixed();
+    const insetBottom = Number(100 - (clipBottom / menu.offsetHeight) * 100).toFixed();
+    const insetLeft = Number(((clipLeft-16) / menu.offsetWidth) * 100).toFixed();
 
-    highlightRef.current.style.left = `${x - 8}px`;
-    highlightRef.current.style.top = `${y - 8}px`;
-    highlightRef.current.style.width = `${w + 24}px`;
-    highlightRef.current.style.height = `${h + 12}px`;
+    menu.style.clipPath = `inset(${insetTop}% ${insetRight}% ${insetBottom}% ${insetLeft}% round 20px)`
   };
 
   const handleClick = (e, link) => {
@@ -72,20 +78,21 @@ export default function Navigation({ links }) {
       scrollDistance += prev;
     }
     menuRef.current.style.transform = `translateY(${scrollDistance}px)`;
+    menuActiveRef.current.style.transform = `translateY(${scrollDistance}px)`;
     setPrevSteps(scrollDistance);
   };
 
   return (
     <>
       <nav className="flex items-start relative" ref={navRef}>
-        <button 
+        {/* <button 
             ref={nameRef}
             className={`menu-btn relative z-20 ${prevActive ? 'highlighted' : ''}`}
             onClick={handleMenuButtonClick}>
                 <span>delutilo.</span>
-            </button>
+            </button> */}
         <ul
-          className={`menu flex flex-col gap-4 items-start z-10 relative ${
+          className={`menu flex flex-col gap-4 items-start z-10 relative pr-4 pl-4 ${
             menuOpen ? 'open' : ''
           }`}
           ref={menuRef}
@@ -105,18 +112,42 @@ export default function Navigation({ links }) {
                   data-order={link.menuOrder}
                   onClick={(e) => handleClick(e, link)}
                   onMouseOver={(e) => highlightMenuItem(e.target)}
-                  onMouseLeave={() =>
-                    highlightMenuItem(navRef.current.querySelector('.active'))
-                  }
+                  // onMouseLeave={() =>
+                  //   highlightMenuItem(navRef.current.querySelector('.active'))
+                  // }
                 >
                   {link.title}
                 </a>
               </li>
             ))}
-          <span
-            className={`slider hidden md:block ${prevActive ? 'highlighted' : ''}`}
-            ref={highlightRef}
-          ></span>
+        </ul>
+        <ul
+          className={`menu menu-active flex flex-col gap-4 items-start pr-4 pl-4 z-20 ${
+            menuOpen ? 'open' : ''
+          }`}
+          ref={menuActiveRef}
+        >
+          {links
+            .sort((a, b) => a.menuOrder - b.menuOrder)
+            .map((link) => (
+              <li
+                className="menu-item"
+                key={link.title}
+              >
+                <a
+                  href={link.href}
+                  className="block"
+                  data-order={link.menuOrder}
+                  onClick={(e) => handleClick(e, link)}
+                  // onMouseOver={(e) => highlightMenuItem(e.target)}
+                  // onMouseLeave={() =>
+                  //   highlightMenuItem(navRef.current.querySelector('.active'))
+                  // }
+                >
+                  {link.title}
+                </a>
+              </li>
+            ))}
         </ul>
       </nav>
       {(
